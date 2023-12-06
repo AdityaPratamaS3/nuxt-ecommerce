5956edd041cc9176c62a0fd8f292ddfc50fac415
AdityaPratamaS3 <193040018.aditya@mail.unpas.ac.id>
2023-12-06 14:54:41 +0700

    first commit

---
+++ components/web/footer.vue
@@ -0,0 +1,145 @@
+<template>
+    <footer class="pt-5" style="background: rgb(255, 255, 255); border-top: 10px solid rgb(11, 49, 157)">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-5 mb-4">
+                    <h4 class="font-weight-bold">TENTANG</h4>
+                    <hr style="border-top: 3px solid rgb(226, 232, 240); border-radius: 0.5rem;">
+                    <img src="/images/logo2.png" width="150"> <span class="ml-2 font-weight-bold"></span>
+                    <p> ASGAR MEBEL, tempatnya furniture eksklusif untuk menciptakan ruang yang indah di rumah anda. Kami
+                        berkomitmen memberikan kualitas terbaik dengan harga yang bersahabat.
+                        jadikan ASGAR MEBEL sebagai pilihan utama anda untuk memenuhi kebutuhan rumah anda.Dengan desain dan
+                        fungsionalitas yang terkini, setiap produk kami dirancang untuk memberikan
+                        kenyamanan dan keindahan pada setiap ruangan.Percayakan kebutuhan furnitur anda pada kami!
+                    </p>
+                </div>
+                <div class="col-md-3 mb-4">
+                    <h4 class="font-weight-bold">METODE PEMBAYARAN</h4>
+                    <hr style="border-top: 3px solid rgb(226, 232, 240); border-radius: 0.5rem;">
+                    <div class="row">
+                        <div class="col-md-4 col-4 mb-3">
+                            <div class="card  rounded">
+                                <div class="card-body p-2 text-center"><img src="/images/payment/BCA.png"
+                                        style="width: 80px;"></div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-4 mb-3">
+                            <div class="card  rounded">
+                                <div class="card-body p-2 text-center"><img src="/images/payment/BNI.png"
+                                        style="width: 70px;"></div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-4 mb-3">
+                            <div class="card  rounded">
+                                <div class="card-body p-2 text-center"><img src="/images/payment/BRI.png"
+                                        style="width: 90px;"></div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-4 mb-3">
+                            <div class="card  rounded">
+                                <div class="card-body p-2 text-center"><img src="/images/payment/GOPAY.png"
+                                        style="width: 80px;"></div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-4 mb-3">
+                            <div class="card  rounded">
+                                <div class="card-body p-2 text-center"><img src="/images/payment/indomaret-logo.png"
+                                        style="width: 80px;">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-4 mb-3">
+                            <div class="card  rounded">
+                                <div class="card-body p-2 text-center"><img src="/images/payment/atm-bersama.jpg"
+                                        style="width: 70px;"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-4">
+                    <h4 class="font-weight-bold">PENGIRIMAN</h4>
+                    <hr style="border-top: 3px solid rgb(226, 232, 240); border-radius: 0.5rem;">
+                    <div class="d-flex align-items-center">
+                        <img src="/images/jne.png" width="60" class="mr-2" alt="JNE Logo">
+                        <span class="ml-2 font-weight-bold"></span>
+                    </div>
+                    <div class="d-flex align-items-center mt-3">
+                        <img src="/images/jnt.png" width="80" class="mr-2" alt="J&T Logo">
+                        <span class="ml-2 font-weight-bold"></span>
+                    </div>
+
+                    <hr style="border-top: 3px solid rgb(226, 232, 240); border-radius: 0.5rem;">
+                    <h5 class="font-weight-normal mt-3">Butuh Bantuan?</h5>
+                    <p>Kamu dapat menghubungi layanan pelanggan kami di:</p>
+                    <!-- <div class="card">
+
+                        <div class="d-flex align-items-center">
+                            <img src="/images/wa.png" width="60" class="mr-2" alt="WhatsApp Logo">
+                            <span class="ml-2 font-weight-bold">081573242937</span>
+                        </div>
+                        <div class="d-flex align-items-center mt-4 mr-5">
+                            <img src="/images/email.png" width="30" class="mr-2" alt="Email Logo">
+                            <span class="ml-3 font-weight-bold">Asgarmebel@gmail.com</span>
+                        </div>
+                    </div> -->
+                    <!-- <div class="row">
+                        <div class="col-sm-6 mb-3 mb-sm-0">
+                            <div class="card">
+                                <div class="card-body">
+
+                                    <h5 class="card-title"><img src="/images/wa.png" width="50" alt="Email Logo"></h5>
+                                    <p class="card-text">081573242937</p>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="card">
+                                <div class="card-body">
+
+                                    <h5 class="card-title"><img src="/images/email.png" width="30" alt="Email Logo"></h5>
+                                    <p class="card-text">Asgarmebel@gmail.com</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div> -->
+                    <div class="row">
+                        <div class="col-sm-6 mb-3 mb-sm-0 order-sm-2">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title"><img src="/images/wa.png" width="35" alt="WhatsApp Logo"></h5>
+                                    <p class="card-text" style="font-weight: 100;">081573242937</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-6 order-sm-1">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title"><img src="/images/email.png" width="30" alt="Email Logo"></h5>
+
+                                    <p class="card-text" style="font-weight: 100;">Asgarmebel@gmail.com</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="row text-center mt-3 pb-3">
+                <div class="col-md-12">
+                    <hr> Copyright©2023.<strong>AsgarMebel.shop</strong>-APS
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+  
+<script>
+export default {
+
+}
+</script>
+  
+<style></style>
---
+++ layouts/auth.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="c-app flex-row align-items-center">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-4">
+
+                    <Nuxt />
+
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+  
+<script>
+export default {
+
+
+}
+</script>
+  
+<style>
+body {
+    font-family: 'Quicksand', sans-serif;
+    font-size: initial !important;
+}
+
+.form-control {
+    height: initial;
+    font-size: 1rem;
+}
+
+.c-app {
+    /* background-color: #333333 !important; */
+    background-image: linear-gradient(63.1deg, rgba(5, 23, 111, 1) 16.4%, rgba(24, 95, 240, 1) 64.5%);
+}
+</style>
